64424b8e2d3801f5fb8f98b29a2101813ad570b4
ArjunPrakash22 <arjunprakashas03@gmail.com>
2024-08-24 21:22:03 +0530

    admin page path correction

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import './App.css';
-import { Login, Register,Dashboard } from "./Pages";
-import AdminPanel from "./AdminPanel";
+import { Login,
+  Register,
+  Dashboard,
+AdminPanel } from "./Pages";
 
 function App() {
   const location = useLocation();
